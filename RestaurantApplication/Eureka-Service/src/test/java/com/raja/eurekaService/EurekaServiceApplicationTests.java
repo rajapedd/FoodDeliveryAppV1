a1b8911ac8e3ae11fc3047e5c3411b97d43f1eee
rajapedd <rajapeddanna.kadavakallu@gmail.com>
2024-07-28 21:41:07 +0530

    Eureka Service Commit

---
+++ RestaurantApplication/Eureka-Service/src/test/java/com/raja/eurekaService/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raja.eurekaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
